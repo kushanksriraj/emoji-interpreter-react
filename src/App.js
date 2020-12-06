7b037299af7c243ca247999eda1ced140ead3d34
Kushank Sriraj <kushanksriraj@gmail.com>
2020-12-06 18:43:06 +0530

    added emojiDict{}

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from "react";
 import "./styles.css";
 
+var inputStyles = {
+  padding : "1rem",
+  width  :  "80%",
+  border: "2px solid",
+  borderRadius : "11px",
+  fontSize : "larger",
+  textAlign : "center"
+}
+
+
+var emojiDict = {
+
+ "🦊" : "Fox",
+ "🦄" : "Unicorn",
+ "🐼" : "Panda",
+ "🐟" : "Fish",
+ "🐞" : "Lady Beetle",
+ "🦉" : "Owl",
+ "🐇" : "Rabbit",
+ "🐶" : "Dog",
+ "🦁" : "Lion",
+ "🦢" : "Swan"
+}
+
+
+
 export default function App() {
   return (
     <div className="App">
       <h1>Animals Emoji</h1>
       <input 
-      placeholder="Enter some animal emoji" />
+      placeholder="Enter some animal emoji" 
+      style={inputStyles}
+      />
     </div>
   );
 }
