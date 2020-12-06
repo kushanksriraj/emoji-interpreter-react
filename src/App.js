4e625802380c1b2ab7132cd6fb54869a9dbf242a
Kushank Sriraj <kushanksriraj@gmail.com>
2020-12-06 19:36:47 +0530

    added emojis and emojiClickHandler

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./styles.css";
 
 var inputStyles = {
-  marginTop : "1rem",
+  marginTop: "1rem",
   padding: "1rem",
   width: "80%",
   border: "2px solid",
@@ -11,13 +11,11 @@ var inputStyles = {
   textAlign: "center"
 };
 
-
 var spanStyle = {
   margin: "auto",
-  padding : "1rem",
-  fontSize : "3rem"
-}
-
+  padding: "1rem",
+  fontSize: "3rem"
+};
 
 var emojiDict = {
   "🦊": "Fox",
@@ -32,43 +30,57 @@ var emojiDict = {
   "🦢": "Swan"
 };
 
+// get list of emojis from the dict
 var emojiList = Object.keys(emojiDict);
 
 export default function App() {
-
   const [emojiMeaning, setEmojiMeaning] = useState("");
 
   function inputEventHandler(event) {
-
     var inputEmoji = event.target.value;
 
-    if (inputEmoji in emojiDict) {
-
-      setEmojiMeaning(emojiDict[inputEmoji]);
-
+    // if text is cleared, reset the emojiMeaning to empty string
+    if (inputEmoji === "") {
+      setEmojiMeaning("");
     } else {
-
-      setEmojiMeaning("Sorry, this emoji is not in the database.");
+      if (inputEmoji in emojiDict) {
+        setEmojiMeaning(emojiDict[inputEmoji]);
+      } else {
+        setEmojiMeaning("Sorry, this emoji is not in the database.");
+      }
     }
+  }
+
+  function emojiClickHandler(item) {
+    var emojiClicked = item;
+    var meaning = emojiDict[emojiClicked];
+    setEmojiMeaning(meaning);
   }
 
   return (
     <div className="App">
       <h1>Animals Emoji</h1>
+
       <input
         placeholder="Enter some animal emoji"
         style={inputStyles}
         onChange={inputEventHandler}
       />
+
       <div className="showMeaning">{emojiMeaning}</div>
 
-      {
-         emojiList.map(item => {
-           return <span key={item}
-           style={spanStyle}> {item} </span>
-         })
-      }
-
+      {emojiList.map((item) => {
+        return (
+          <span
+            key={item}
+            style={spanStyle}
+            onClick={() => emojiClickHandler(item)}
+          >
+            {" "}
+            {item}{" "}
+          </span>
+        );
+      })}
     </div>
   );
 }
